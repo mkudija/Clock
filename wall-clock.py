de74d645e5a6b2be11cd23572b35e84a2e48d1c8
Matthew Kudija <m.kudija@gmail.com>
2016-11-15 07:01:42 -0500

    clean code (move to functions), add images

--- wall-clock.py
+++ wall-clock.py
@@ -1,22 +1,12 @@
-"""
-Demo of a line plot on a polar axis.
-"""
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-#r = np.arange(0, 3.0, 0.01)
-#theta = 2 * np.pi * r
-
+# set up plot and define constants
 ax = plt.subplot(111, projection='polar')
-#ax.text(0, 1, r'an equation: $E=mc^2$', fontsize=15)
-
 numbers = range(0,24)
-#numbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
 rmax = 100
 r = rmax*.85
 l_dash = rmax*.08
-
 
 # print outer numbers (24-hour)
 idx = -np.pi/2
@@ -29,7 +19,6 @@ idx = np.pi/2 - 2*np.pi/len(numbers)
 for number in numbers[1:13]:
 	ax.text(idx,r*.89,str(numbers[number]),ha='center',va='center',weight='bold',color='red')
 	idx -= 2*np.pi/len(numbers)
-
 
 # print hour tick marks
 idx = -np.pi/2
@@ -52,9 +41,63 @@ for number in range(0,len(numbers_stick)):
 # can make all ticks with this if we use "if" with right conditions
 
 
+def plot_night(sunrise_hr, sunrise_min, sunset_hr, sunset_min, color):
+	sunrise_hr_neg = sunrise_hr * -1
+	sunrise_min_neg  = sunrise_min * -1
+
+	hr_pi = sunrise_hr_neg * 2*np.pi/24 
+	hr_min = (hr_pi + sunrise_min_neg * 2*np.pi/(24*60) ) - np.pi/2
+
+	sunrise_time = sunrise_hr + sunrise_min/60
+	sunset_time  = sunset_hr  + sunset_min/60
+
+	night_time = 24 - (sunset_time - sunrise_time)
+	night_time_rad = night_time * 2*np.pi/24
+
+	angles = [hr_min,hr_min]
+	ax.bar(angles, np.full(2, 100), width=night_time_rad, bottom=0.0, color=color, linewidth=0)
+
+def plot_city(sunrise_dec, sunset_dec, sunrise_jun, sunset_jun, city):
+	plot_night(sunrise_dec[0], sunrise_dec[1], sunset_dec[0], sunset_dec[1], color='#E5E3E2')
+	plot_night(sunrise_jun[0], sunrise_jun[1], sunset_jun[0], sunset_jun[1], color='#959493')
+
+	# print name and location
+	ax.text(90*(np.pi/180),20,city,ha='center',va='center',weight='bold',fontsize=8)
+
+	ax.set_rmax(rmax)
+	ax.axes.get_xaxis().set_visible(False)
+	ax.axes.get_yaxis().set_visible(False)
+	ax.grid(False)
+
+	plt.savefig('clock-' + city + '.png', dpi=500)
+	plt.show()
+
+'''
+city = 'Columbus, OH'
+sunrise_dec = [7, 50]
+sunset_dec  = [17, 10]
+sunrise_jun = [6, 3]
+sunset_jun  = [21, 3]
+
+'''
+city = 'Paso Robles, CA'
+sunrise_dec = [7, 8]
+sunset_dec  = [16, 52]
+sunrise_jun = [5, 47]
+sunset_jun  = [20, 21]
+
+
+plot_city(sunrise_dec, sunset_dec, sunrise_jun, sunset_jun, city)
 
 
 
+
+
+
+
+
+
+'''
 #-------------------------------------------------------------
 city = 'Paso Robles, CA'
 
@@ -67,7 +110,7 @@ sunset_hr  = 16
 sunset_min = 52
 #--------------------------------------
 
-'''
+
 #-------------------------------------------------------------
 city = 'Columbus, OH'
 
@@ -79,7 +122,7 @@ sunrise_min = 50
 sunset_hr  = 17
 sunset_min = 10
 #--------------------------------------
-'''
+
 
 sunrise_hr_neg = sunrise_hr * -1
 sunrise_min_neg  = sunrise_min * -1
@@ -107,7 +150,7 @@ sunset_hr  = 20
 sunset_min = 21
 #--------------------------------------
 
-'''
+
 # JUNE
 #city = 'Columbus, OH'
 #--------------------------------------
@@ -117,7 +160,7 @@ sunrise_min = 3
 sunset_hr  = 21
 sunset_min = 3
 #--------------------------------------
-'''
+
 
 sunrise_hr_neg = sunrise_hr * -1
 sunrise_min_neg  = sunrise_min * -1
@@ -146,3 +189,4 @@ ax.grid(False)
 
 plt.savefig('clock-' + city + '.png', dpi=300)
 plt.show()
+'''
